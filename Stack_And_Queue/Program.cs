1eae161df2f91ff5734395bfbc4253bbc9ae9922
Mrhaltung <dipeshchandekar99@gmail.com>
2021-11-17 23:13:33 +0530

    Created a Queue

---
+++ Stack_And_Queue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_And_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+
+            foreach (int item in queue)
+            {
+                Console.WriteLine("Pushing the Elements in Queue.");
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----------------------------------\n");
+        }
+    }
+}
